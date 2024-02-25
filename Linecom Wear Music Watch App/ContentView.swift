66c35b8b0da48a84fe921c03d830b2d0a8f0905c
Linecom-lik <3448336734@qq.com>
2024-02-25 20:27:02 +0800

    Initial Commit

---
+++ Linecom Wear Music Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Linecom Wear Music Watch App
+//
+//  Created by 澪空 on 2024/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Linecom Wear Music Watch App/Linecom_Wear_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Linecom_Wear_MusicApp.swift
+//  Linecom Wear Music Watch App
+//
+//  Created by 澪空 on 2024/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Linecom_Wear_Music_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
